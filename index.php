f577a2b91387f400922f49d0341b37b75289d6be
Mon <91776290+ramonnapa28@users.noreply.github.com>
2023-11-08 21:31:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Rail Routes</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">Rail Routes</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">Make Reservation</a></li>
+								<li><a href="employee/emp-login.php">Employee Login</a></li>
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon solid fa-train"></span></div>
+							<h2>Rail Routes</h2>
+							<p>Simple, Effective, and User Friendly Way To Reserve Train Tickets Effortlessly</a></p>
+						</div>
+					</section>
+
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Get in touch</h2>
+							
+							<ul class="contact">
+								<li class="icon solid fa-home">
+									Rail Routes<br />
+									PHP Project<br />
+									
+								</li>
+								<li class="icon solid fa-phone">(+254) 127-0000</li>
+								<li class="icon solid fa-envelope"><a href="#">mail@orrs.com</a></li>
+								<li class="icon brands fa-twitter"><a href="#">twitter.com/Orrs</a></li>
+								<li class="icon brands fa-facebook-f"><a href="#">facebook.com/orrs</a></li>
+								<li class="icon brands fa-instagram"><a href="#">instagram.com/orrs</a></li>
+							</ul>
+							<ul class="copyright">
+								<li>&copy; 2022 - 2023 </li><li> Rail Routes | Final Project </li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
